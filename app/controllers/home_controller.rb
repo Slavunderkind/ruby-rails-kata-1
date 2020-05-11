bfabd067d5d55d4af6569de9f0af8dbd83ae44cc
slavunderkind <smcankova@gmail.com>
2020-05-11 16:25:48 +0300

    Change Script#all

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
+require 'byebug'
 class HomeController < ApplicationController
+
+  def index
+    @scripts = Script.new.all
+  end
 end
--- app/models/script.rb
+++ app/models/script.rb
@@ -1,3 +1,7 @@
 class Script
   KINDS = %w[book magazine].freeze
+
+  def all
+    Book.all + Magazine.all
+  end
 end
