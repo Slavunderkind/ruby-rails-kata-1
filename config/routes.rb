61a68ce321a86b84185f2c0598ac953fbc505540
slavunderkind <smcankova@gmail.com>
2020-05-11 18:36:29 +0300

    Add post and gets routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
+  post '/search_by_isbn' => 'home#search_by_isbn', param: :isbn
   get '/search_by_isbn' => 'home#search_by_isbn'
+
+  post '/search_by_authors_email' => 'home#search_by_authors_email', param: :authors_email
   get '/search_by_authors_email' => 'home#search_by_authors_email'
   get '/sort' => 'home#sort'
 end
