a33b1427a3d5eb0322825477e572f889f321bbe2
slavunderkind <smcankova@gmail.com>
2020-05-11 18:16:20 +0300

    Rename @script with @script_obj

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,32 +1,32 @@
 require 'byebug'
 class HomeController < ApplicationController
-  before_action :set_script
+  before_action :set_script_obj
 
   def index
-    @scripts = @script.all
+    @scripts = @script_obj.all
   end
 
   def search_by_isbn
-    @script = @script.by_isbn(params[:isbn])
+    @script = @script_obj.by_isbn(params[:isbn])
 
     render :show
   end
 
   def search_by_authors_email
-    @scripts = @script.by_authors_email(params[:authors_email])
+    @scripts = @script_obj.by_authors_email(params[:authors_email])
 
     render :scripts
   end
 
   def sort
-    @scripts = @script.all_sorted_by_title
+    @scripts = @script_obj.all_sorted_by_title
 
     render :scripts
   end
 
   private
 
-  def set_script
-    @script = Script.new
+  def set_script_obj
+    @script_obj = Script.new
   end
 end
