3b44d61ebf554ba94cec4f772716c5a7122942ac
slavunderkind <smcankova@gmail.com>
2020-05-11 16:51:12 +0300

    Add #search_by_isbn and #set_script

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 require 'byebug'
 class HomeController < ApplicationController
+  before_action :set_script
 
   def index
-    @scripts = Script.new.all
+    @scripts = @script.all
   end
 
   def search_by_isbn
+     @script.by_isbn(params[:isbn])
+  end
+
+  private
+
+  def set_script
+    @script = Script.new
   end
 end
