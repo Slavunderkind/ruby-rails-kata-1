706edf4ec63c3f7f578b970f11944631e1443a50
slavunderkind <smcankova@gmail.com>
2020-05-11 18:01:21 +0300

    Config FactoryBot

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,8 +1,8 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :book do
-    title "Harry Potter"
-    isbn  "1111-1111-1111"
-    authors_emails "j.k.rowling@gmail.com"
-    description "Harry Potter is a series of fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry."
+    title { "Harry Potter" }
+    isbn  { "1111-1111-1111" }
+    authors_emails { "j.k.rowling@gmail.com" }
+    description { "Harry Potter is a series of fantasy novels written by British author J. K. Rowling. The novels chronicle the lives of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry." }
   end
 end
---
+++ spec/models/script_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Script, type: :model do
+
+  describe '#all' do
+    subject { Script.new.all }
+
+    context 'when only books exists' do
+      let(:book) { create(:book) }
+
+      it 'returns all records of books' do
+        expect(subject).to eq([book])
+      end
+    end
+  end
+
+  describe '#by_isbn' do
+  end
+  describe '#by_authors_email' do
+  end
+  describe '#all_sorted_by_title' do
+  end
+end
