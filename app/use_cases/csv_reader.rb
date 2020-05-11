1728323a0d9defa3607aab1cd190715d391b13b6
slavunderkind <smcankova@gmail.com>
2020-05-11 15:25:22 +0300

    Pass col_sep to CSV.read function since separator is different than default

--- app/use_cases/csv_reader.rb
+++ app/use_cases/csv_reader.rb
@@ -3,11 +3,12 @@ require 'csv'
 class CsvReader
 
   APP_PATH = File.dirname(File.dirname(File.expand_path(__FILE__))).freeze
+  SEPARATOR = "\;"
 
   def read_authors
     filename = "#{APP_PATH}/data/authors.csv"
 
-    CSV.read(filename).each do |row|
+    CSV.read(filename, col_sep: SEPARATOR).each do |row|
       next if index.zero?
 
       #Save record into DB
