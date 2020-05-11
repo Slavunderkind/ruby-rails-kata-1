9a8b96e9bb5445400ec3f0825c0d52a6697ffc59
slavunderkind <smcankova@gmail.com>
2020-05-11 15:34:05 +0300

    Add rubocop gem. Save records into Author model from #read_authors

--- app/use_cases/csv_reader.rb
+++ app/use_cases/csv_reader.rb
@@ -1,17 +1,23 @@
+# frozen_string_literal: true
+
 require 'csv'
 
+# Read and save data from CSV file into DB
 class CsvReader
-
   APP_PATH = File.dirname(File.dirname(File.expand_path(__FILE__))).freeze
   SEPARATOR = "\;"
 
   def read_authors
     filename = "#{APP_PATH}/data/authors.csv"
 
-    CSV.read(filename, col_sep: SEPARATOR).each do |row|
+    CSV.read(filename, col_sep: SEPARATOR).each_with_index do |row, index|
       next if index.zero?
 
-      #Save record into DB
+      Author.create!(
+        email: row[0],
+        first_name: row[1],
+        last_name: row[2]
+      )
     end
   end
 end
