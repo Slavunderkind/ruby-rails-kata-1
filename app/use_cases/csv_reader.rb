cf22d365ddd01acc20f9af3f88b728b9aa54759e
slavunderkind <smcankova@gmail.com>
2020-05-11 15:23:14 +0300

    Add #read_authors

--- app/use_cases/csv_reader.rb
+++ app/use_cases/csv_reader.rb
@@ -1,2 +1,16 @@
+require 'csv'
+
 class CsvReader
+
+  APP_PATH = File.dirname(File.dirname(File.expand_path(__FILE__))).freeze
+
+  def read_authors
+    filename = "#{APP_PATH}/data/authors.csv"
+
+    CSV.read(filename).each do |row|
+      next if index.zero?
+
+      #Save record into DB
+    end
+  end
 end
